364eac92ae144e3c2cd2d70d68c26447363c56e8
klarkcharlz <klark.charlz@mail.ru>
2021-09-28 01:19:07 +0300

    [upd]

--- backened/users/views.py
+++ backened/users/views.py
@@ -1,29 +1,10 @@
-from rest_framework.generics import get_object_or_404
 from .models import User
 from .serializers import UserModelSerializer
-from rest_framework.response import Response
 from rest_framework import viewsets
+from rest_framework import mixins
 
 
-class UserViewSet(viewsets.ViewSet):
-    def list(self, request):
-        users = User.objects.all()
-        serializer = UserModelSerializer(users, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        user = get_object_or_404(User, pk=pk)
-        serializer = UserModelSerializer(user)
-        return Response(serializer.data)
-
-    def update(self, request, pk=None):
-        User.objects.filter(pk=pk).update(**request.data)
-        user = get_object_or_404(User, pk=pk)
-        serializer = UserModelSerializer(user)
-        return Response(serializer.data)
-
-    def partial_update(self, request, pk=None):
-        User.objects.filter(pk=pk).update(**request.data)
-        user = get_object_or_404(User, pk=pk)
-        serializer = UserModelSerializer(user)
-        return Response(serializer.data)
+class UserViewSet(mixins.UpdateModelMixin, mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer
